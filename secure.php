bc33f5695624a3e11ee5dab666002f789e07f799
itsrishre <ydeep24@yahoo.com>
2013-03-09 23:22:51 +0530

    Update secure.php

--- secure.php
+++ secure.php
@@ -9,6 +9,6 @@ else
 {
 	$result=$db->query("SELECT credits FROM users WHERE username='{$_SESSION['username']}'");
 	$row=$result->fetch_array();
-	$_SESSION['credits']=$row[0];
+	$_SESSION['credits']=100;
 	$images=$db->query("SELECT url from images WHERE `to`='{$_SESSION['username']}'");
 }
